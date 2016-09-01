29abc2e077c898d54a58d294dd7ebb9a5f9eef0c
HorridTom <woodcock.thomas@gmail.com>
2016-09-01 19:12:02 +0100

    Refactored SPCIOTest as parameterized test.

--- SPCalgorithm1/test/uk/ac/ic/doc/wishnwl/tw/SPCIOTest.java
+++ SPCalgorithm1/test/uk/ac/ic/doc/wishnwl/tw/SPCIOTest.java
@@ -1,8 +1,7 @@
 package uk.ac.ic.doc.wishnwl.tw;
 
-import static org.junit.Assert.*;
-
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Iterator;
@@ -10,125 +9,135 @@ import java.util.Vector;
 import java.io.File;
 
 import org.eclipse.birt.data.engine.core.DataException;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-
+@RunWith(value = Parameterized.class)
 public class SPCIOTest {
-
-	@Test
-	public void testAnalyseCsv() {
-
-		//Get a list of all the files in the testdata folder
+	
+	String dataFileName;
+	SPCIO testIO;
+	SPCIO correctIO;
+	
+	@Before
+	public void initialize() {
+		this.testIO = new SPCIO();
+		this.correctIO = new SPCIO();
+	}
+	
+	//Constructor takes parameter from list and passes to test
+	public SPCIOTest (String fileName) {
+		this.dataFileName = fileName;
+	}
+	
+	//Returns a list of data files to test the algorithm on.
+	//For each file in the list there must be a pre-analysed output file
+	//in the same folder, against which the algorithm's output
+	//will be tested.
+	@Parameterized.Parameters
+	public static Collection<Object[]> dataSets() {
+		Collection<Object[]> fileListResult = new ArrayList<Object[]>();
+		String fileName;
 		File folder = new File("C:\\Users\\tw299\\git\\spc-algorithm\\SPCalgorithm1\\testdata");
 		List<File> listOfFiles = new ArrayList<>(Arrays.asList(folder.listFiles()));
-		String fileName = new String();
-		String testOutputFileName = new String();
-		String correctOutputFileName = new String();
-		int count = 0;
 
 		//Remove any files from the list where their filename ends with "OUT.csv"
 		// - this should just leave the raw test data files in the list
 		for (Iterator<File> iter = listOfFiles.iterator(); iter.hasNext();) {
 			fileName = iter.next().getAbsolutePath();
-
-			//System.out.println(fileName.substring(fileName.length()-7));
 			if (fileName.length()>7 && fileName.substring(fileName.length()-7).equals("OUT.csv")) {
-				//System.out.println("Yes");
 				iter.remove();
 			}
 		}
-
+		
+		// Populate the output Collection with the filenames
 		for (Iterator<File> iter = listOfFiles.iterator(); iter.hasNext();) {
-
-			Vector<Double[]> result = new Vector<Double[]>();
-
-			//Get the next filename in the list of test data files
 			fileName = iter.next().getAbsolutePath();
-			//System.out.println(fileName);
-
-			//Run the current algorithm on fileName...
-			try {
-				result = SPCIO.analyseCsv(fileName);
-			} catch (DataException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				fail("Data Exception.");
-			}
-
-			//...and save the result.
-			//TODO: Shouldn't have to pass the maximum number of iterations to saveSpcToCsv method
-			//TODO: Consider making saveSpcToCsv return the actual file name used if successful?
-			SPCIO.saveSpcToCsv(fileName, "TEST", 0, result);
-
-			//Reconstruct the filename that the test result will have been saved as.
-			testOutputFileName = fileName.substring(0, fileName.length() - 4) + "_" + "TEST" + "_OUT.csv";
-			correctOutputFileName = fileName.substring(0, fileName.length() - 4) + "_endresult_OUT.csv";
-
-			//Load in the results of the test run...
-			SPCIO testIO = new SPCIO();
-			testIO.loadCsv(testOutputFileName);
-			//testIO.makeVector();
-
-			//...and the original results they should match.
-			SPCIO correctIO = new SPCIO();
-			correctIO.loadCsv(correctOutputFileName);
-			//correctIO.makeVector();
-
-			//Assert that the test run gave the same results as the original.
-			assertTrue(csvContentEqual(testIO.myEntries,correctIO.myEntries));
-
-			count++;
-			result = null;
+			String[] paramArray = new String[1];
+			paramArray[0] = fileName;
+			fileListResult.add(paramArray);
+		}
+		// Return the list of filenames to be used as parameters for the test.
+		return fileListResult;
+	}
+	
+	@Test
+	public void testSPCIO() {
+		String testOutputFileName = new String();
+		String correctOutputFileName = new String();
+		Vector<Double[]> result = new Vector<Double[]>();
+		
+		//Run the current algorithm on fileName...
+		try {
+			result = SPCIO.analyseCsv(dataFileName);
+		} catch (DataException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail("Data Exception.");
 		}
 
-		System.out.println(count);
+		//...and save the result.
+		//TODO: Shouldn't have to pass the maximum number of iterations to saveSpcToCsv method
+		//TODO: Consider making saveSpcToCsv return the actual file name used if successful?
+		SPCIO.saveSpcToCsv(dataFileName, "TEST", 0, result);
 
+		//Reconstruct the filename that the test result will have been saved as.
+		testOutputFileName = dataFileName.substring(0, dataFileName.length() - 4) + "_" + "TEST" + "_OUT.csv";
+		correctOutputFileName = dataFileName.substring(0, dataFileName.length() - 4) + "_endresult_OUT.csv";
+
+		//Load in the results of the test run...
+		testIO.loadCsv(testOutputFileName);
+
+		//...and the original results they should match.
+		correctIO.loadCsv(correctOutputFileName);
+		//correctIO.makeVector();
+
+		//Assert that the test run gave the same results as the original.
+		assertTrue(csvContentEqual(testIO.myEntries,correctIO.myEntries));
+		
 	}
-
+	
+	//Method to test whether the contents of two csv files are the same.
 	private boolean csvContentEqual(List<String[]> myEntries,
 			List<String[]> myEntries2) {
-
-		
+		// If there are a different number of rows, the two csv files
+		// cannot be equal.
 		if (myEntries.size() != myEntries2.size()) return false;
-		//
+
 		int i;
 		int l;
 		int l2;
-		//int j = 0;
 		String[] a, a2;
 		
 		Iterator<String[]> iter = myEntries.iterator();
 		Iterator<String[]> iter2 = myEntries2.iterator();
 				
 		while (iter.hasNext() && iter2.hasNext()) {
-			//j++;
-			//System.out.println("Index: " + j);
 			
 			a = iter.next();
 			a2 = iter2.next();
 			
-			
-			//System.out.println("Next Array (1)..." + iter.next().length);
 			l = a.length;
 			l2 = a2.length;
 			
+			// If there are a different number of entries in a given row, the
+			// two csv files cannot be equal.
 			if (l != l2) return false;
 			
 			for (i=0; i < l; i++) {
-				
-				//System.out.println(i + ": " + a[i] + ", " + a2[i]);
-				//System.out.println(a[i].equals(a2[i]));
+				// If any individual cell differs, the two csv files cannot
+				// be equal.
 				if (!a[i].equals(a2[i])) return false;
-				
-				//System.out.println(i);
-				//System.out.println(iter.next()[i]);
+
 			}
-			
-			
 		}
-		
-		//System.out.println("End");
+		// If we haven't already returned false, it must be the case that the
+		// two csv files have the same contents.
 		return true;
 	}
-
 }

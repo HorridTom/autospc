6d1b9d4240d58e01105c3e341445fc7552a82639
HorridTom <woodcock.thomas@gmail.com>
2018-03-14 10:16:41 +0000

    Tests for the algorithm itself, within SPCCalculator

---
+++ SPCalgorithm1/test/uk/ac/ic/doc/wishnwl/tw/AllTests.java
@@ -0,0 +1,11 @@
+package uk.ac.ic.doc.wishnwl.tw;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ SPCCalculatorTest.class, SPCIOTest.class })
+public class AllTests {
+
+}

d553a4bb824ea1968d1c029a87bde715441917f9
HorridTom <woodcock.thomas@gmail.com>
2021-08-24 14:49:12 +0100

    actually pass an integer!

--- tests/testthat/test_integer_x.R
+++ tests/testthat/test_integer_x.R
@@ -8,7 +8,8 @@ testthat::test_that("plot_auto_SPC does not throw an error with integer x values
                               70, 68, 43, 76, 98, 108, 79, 92, 84, 76, 69, 88, 83, 101, 81, 
                               72, 89, 90, 81, 82, 68, 82, 84, 93, 79, 87)),
                    row.names = c(NA, -50L),
-                   class = c("tbl_df", "tbl", "data.frame"))
+                   class = c("tbl_df", "tbl", "data.frame")) %>%
+    dplyr::mutate(y = as.integer(y))
   # Expect no error when calling plot_auto_SPC with this data
   expect_error(
     plot_auto_SPC(df1, title = "my title", subtitle = "my subtitle"),

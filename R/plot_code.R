1b48ca1c6f38d1ea4788bf15045385e67c2af873
Imogen Connor-Helleur <imooconnor@outlook.com>
2021-09-13 17:27:19 +0100

    Plot function can now handle x column being input as date, POSIXct, integer or double.

--- R/plot_code.R
+++ R/plot_code.R
@@ -60,8 +60,7 @@ plot_auto_SPC <- function(df,
   #get type from x variable so that ggplot axes are correct
   #currently only accepting Date, numeric and integer as acceptable types
   xType <- class(df$x)
-  print(xType)
-  if(xType != "Date" & xType!= "POSIXct" & xType != "numeric" & xType != "integer"){
+  if(xType != "Date" & all(xType!= c("POSIXct", "POSIXt")) & xType != "numeric" & xType != "integer"){
     print("Please make sure that your x column is a 'Date', 'numeric' or 'integer' type.")
   }
   
@@ -87,6 +86,14 @@ plot_auto_SPC <- function(df,
     #(i.e. so there isn't a gap between calculation and display limits)
     dplyr::mutate(limitChange = ifelse(periodType == dplyr::lag(periodType), F, T)) #%>%
     #mutate(periodType = ifelse(limitChange & periodType == "calculation", lag(periodType), periodType)) 
+    
+    
+  #re-convert x column back to date if necessary
+  if(xType == "Date" | xType == "POSIXct" | xType == "POSIXt"){
+    df <- df %>%
+      dplyr::mutate(x = as.Date(x))
+  }
+  
   
   #store break points as vector
   breakPoints <- which(df$breakPoint)
@@ -98,6 +105,7 @@ plot_auto_SPC <- function(df,
                                            highlight))
   }
   
+  #create initial plot object without formatting 
   pct <- ggplot2::ggplot(df, ggplot2::aes(x,y))
   
   # chart y limit
@@ -115,7 +123,6 @@ plot_auto_SPC <- function(df,
   #start and end dates
   start_x <- min(df$x, na.rm = T)
   end_x <- max(df$x, na.rm = T)
-  print(start_x)
   
   #get y limit
   if(!is.null(override_y_lim)){
@@ -135,9 +142,6 @@ plot_auto_SPC <- function(df,
     caption <- paste(chartType,"Shewhart Chart.","\n*Shewhart chart rules apply (see Understanding the Analysis tab for more detail) \nRule 1: Any point outside the control limits \nRule 2: Eight or more consecutive points all above, or all below, the centre line")
 
     p <- format_SPC(pct, df = df, r1_col = r1_col, r2_col = r2_col) +
-      # ggplot2::scale_x_date(labels = scales::date_format("%Y-%m-%d"),
-      #                       breaks = seq(start_x, end_x, x_break),
-      #                       limits = c(start_x, end_x)) +
       ggplot2::ggtitle(title, 
                        subtitle = subtitle) +
       ggplot2::labs(x = "Day", 
@@ -155,29 +159,23 @@ plot_auto_SPC <- function(df,
       ggplot2::annotate("text", 
                         x = df$x[breakPoints] + 2, 
                         y = df$ucl[breakPoints] + ucl_start/annotation_dist_fact, 
-                        label = round(df$cl[breakPoints])) #+ 
-      # ggplot2::scale_x_continuous(breaks = seq(start_x, end_x, 7),
-      #                           limits = c(start_x, end_x))
+                        label = round(df$cl[breakPoints])) 
     
     #formats x axis depending on x type
     if(xType == "Date" | xType == "POSIXct" | xType == "POSIXt"){
-      df <- df %>% dplyr::mutate(x = as.Date(x))
-
+      
       #get x axis breaks
-      print("got here date")
       x_break <- dplyr::if_else(is.null(x_break),
                                 as.numeric(difftime(as.Date(end_x), as.Date(start_x), units = "days")) / 40,
                                 x_break)
-      print(x_break)
 
-      p <- p + ggplot2::scale_x_date(#labels = scales::date_format("%Y-%m-%d")#,
-                                         #breaks = seq(as.Date(start_x), as.Date(end_x), x_break),
-                                         #limits = c(as.Date(start_x), as.Date(end_x))
+      p <- p + ggplot2::scale_x_date(labels = scales::date_format("%Y-%m-%d"),
+                                     breaks = seq(as.Date(start_x), as.Date(end_x), x_break),
+                                     limits = c(as.Date(start_x), as.Date(end_x))
                                      )
 
     }else if(xType == "integer"){
       #get x axis breaks
-      print("got here integer")
       x_break <- dplyr::if_else(is.null(x_break),
                                 (end_x - start_x) / 40,
                                 x_break)
@@ -186,7 +184,6 @@ plot_auto_SPC <- function(df,
                                            limits = c(start_x, end_x))
     }else{
       #get x axis breaks
-      print("got here other")
       x_break <- dplyr::if_else(is.null(x_break),
                                 (end_x - start_x) / 40,
                                 x_break)

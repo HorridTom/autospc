98b77ca488304eb19adaba2a3bac32bbe7790b78
Comfort <Niffieoz@yahoo.com>
2023-07-12 12:37:00 +0100

    test to check that the mr limits function match the limits from qicharts2

---
+++ tests/testthat/test_mr_chart.R
@@ -0,0 +1,13 @@
+#load test data 
+mr_data <- readRDS("testdata/test_mr_data.rds")
+
+#test that mr control limits match the recalculated mr control limits
+#tolerance = 0.01
+library(testthat)
+test_that("mR limits match the recalculated mR limits",{
+  results <- get_mr_limits(y = mr_data$y)
+  
+  correct_answers <- qicharts2::qic(y, data = mr_data, chart = 'mr', return.data = TRUE)
+  expect_equal(results$cl, correct_answers$cl)
+  expect_equal(results$ucl, correct_answers$ucl)
+})

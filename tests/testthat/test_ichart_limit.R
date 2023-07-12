3d650d8cb017c2094c76d3b385f21d807787c882
Comfort <Niffieoz@yahoo.com>
2023-07-12 12:21:18 +0100

    Added test for the case when moving ranges are above the ucl_mr
    improved comment on i chart limits test

--- tests/testthat/test_ichart_limit.R
+++ tests/testthat/test_ichart_limit.R
@@ -2,11 +2,24 @@ library(testthat)
 #load in test data
 test_data <- readRDS("testdata/test_individual_data.rds")
 
-#the test itself 
+#test that the i chart limits is the same as qicharts2 results 
 test_that("I chart limits the same as live qicharts2 v.0.7.2",{
   results <- get_i_limits(y = test_data$y)
   
   correct_answers <- qicharts2::qic(x, y, data = test_data, chart = 'i', return.data = TRUE)
+  
+  expect_equal(results$cl, correct_answers$cl)
+  expect_equal(results$lcl, correct_answers$lcl)
+  expect_equal(results$ucl, correct_answers$ucl)
+  
+})
+
+#test that moving range limits that are above ucl_mr are removed 
+extreme_mr_data <- readRDS("testdata/test_mr_data.rds")
+test_that("I chart limits the same as live qicharts2 v.0.7.2",{
+  results <- get_i_limits(y = extreme_mr_data$y)
+  
+  correct_answers <- qicharts2::qic(x, y, data = extreme_mr_data, chart = 'i', return.data = TRUE)
   
   expect_equal(results$cl, correct_answers$cl)
   expect_equal(results$lcl, correct_answers$lcl)

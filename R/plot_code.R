ab2fba50450a646ec000de7a94dc1a387e12b1df
HorridTom <woodcock.thomas@gmail.com>
2024-04-08 17:27:43 +0100

    Add argument to switch off MR chart in XMR

--- R/plot_code.R
+++ R/plot_code.R
@@ -66,7 +66,8 @@ plot_auto_SPC <- function(df,
                           use_caption = TRUE,
                           x_pad_end = NULL,
                           noRecals = FALSE,
-                          showLimits = TRUE
+                          showLimits = TRUE,
+                          showMR = TRUE
 ) { 
   
   df_original <- df
@@ -214,7 +215,7 @@ plot_auto_SPC <- function(df,
     df <- df %>%
       dplyr::mutate(plotPeriod = paste0(periodType, periodStart))
     
-    if(chartType == "XMR") {
+    if((chartType == "XMR") & showMR) {
       mc <- match.call()
       mc[["chartType"]] <- "MR"
       if("title" %in% names(mc)) {mc[["title"]] <- NULL}
@@ -291,7 +292,7 @@ plot_auto_SPC <- function(df,
                                              limits = c(start_x, end_x))
       }
       
-      if(chartType == "XMR") {
+      if((chartType == "XMR") & showMR) {
         p <- p + 
           ggplot2::labs(caption = NULL,
                         x = NULL) + 

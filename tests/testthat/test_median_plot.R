d4dc1472bff90ffceecd8fe6e1a165b770f9f6b5
Comfortti <niffieoz@yahoo.com>
2024-06-26 15:47:48 +0100

    Modified test code. Test for floatingMedian = auto is corrected. Similar expectations combined into one test.

--- tests/testthat/test_median_plot.R
+++ tests/testthat/test_median_plot.R
@@ -5,7 +5,7 @@ library(autospc)
 # Load test data
 test_median_data <- readRDS("testdata/test_median_data.rds")
 
-test_that("the number of medians calculated match the number of data points used for median calculation", {
+test_that("the number of medians being plotted are correctly calculated", {
   # Set the n points for the median 
   test_median_n <- 12L 
 
@@ -15,7 +15,8 @@ test_that("the number of medians calculated match the number of data points used
                                   floatingMedian = "yes",
                                   floatingMedian_n = test_median_n,
                                   showMR = FALSE)
-
+  
+  # View df being used to plot XmR chart 
   chart_result_data <- chart_result$data 
 
   # Extract all median values from the result data  
@@ -23,48 +24,16 @@ test_that("the number of medians calculated match the number of data points used
     dplyr::filter(!is.na(median)) %>% 
     dplyr::pull(median)
 
-  # Test that the length of the test_median__n points matches the length of the 
-  # median points listed 
+  # Test that the length of the test_median__n points matches the length of the median points calculated in the df
   expect_equal(length(result_median),test_median_n)
-})
-
-test_that("only one median is being calculated",{
-  #set the n points for the median 
-  test_median_n <- 12L 
   
-  #view the data being plotted in XmR chart 
-  chart_result <- autospc::plot_auto_SPC(df = test_median_data,
-                                         chartType = 'XMR',
-                                         floatingMedian = "yes",
-                                         floatingMedian_n = test_median_n,
-                                         showMR = FALSE)
-  
-  chart_result_data <- chart_result$data 
-  
-  # Extract all median values from the result data  
-  result_median <- chart_result_data %>% 
-    dplyr::filter(!is.na(median)) %>% 
-    dplyr::pull(median)
-  
+  # Identify how many medians are being calculated 
   unique_result_median <- unique(result_median)
-
+  
+  # Test that only one median is being calculated 
   expect_equal(length(unique_result_median), 1L)
-})
-
-test_that("the calculated median uses the last test_median_n rows", {
-  #set the n points for the median 
-  test_median_n <- 12L 
   
-  #view the data being plotted in XmR chart 
-  chart_result <- autospc::plot_auto_SPC(df = test_median_data,
-                                         chartType = 'XMR',
-                                         floatingMedian = "yes",
-                                         floatingMedian_n = test_median_n,
-                                         showMR = FALSE)
-  
-  chart_result_data <- chart_result$data 
-  
-  # Summaries the list of medians into a singular median value
+  # Summaries the df of medians into a singular median value
   result_median <- chart_result_data %>% 
     dplyr::filter(!is.na(median)) %>% 
     dplyr::summarise(medi = median(median)) %>%
@@ -75,7 +44,8 @@ test_that("the calculated median uses the last test_median_n rows", {
     dplyr::slice((n() - test_median_n + 1):n()) %>%
     dplyr::summarise(med = median(y, na.rm = TRUE)) %>%
     dplyr::pull(med)
-
+  
+  # Test that the median displayed is calculated correctly 
   expect_equal(result_median, correct_median)
 })
 
@@ -94,8 +64,8 @@ test_that("the median is not generated nor plotted when floatingMedian is set to
 })
 
 test_that("the median is plotted after a shift rule 2 break when floatingMedian is set to auto", {
-  # Load test dataset where last floatingMedian_n contains at least 1 shift rule 2 break 
-  test_autoMedian <- readRDS("testdata/test_oppositeRuleBreak_later.rds")
+  # Load test df where last floatingMedian_n contains at least 1 shift rule 2 break 
+  test_autoMedian <- readRDS("testdata/test_medianWithRule2Break.rds")
   
   test_median_n <- 12L 
   
@@ -107,27 +77,18 @@ test_that("the median is plotted after a shift rule 2 break when floatingMedian 
   
   auto_medianResult_data <- auto_medianResult$data 
   
-  # Check that both the median and rule2 columns exist 
+  # Check that the median is calculated when there is a shift rule 2 break in the last floatingMedian_n points 
   expect_true("median" %in% names(auto_medianResult_data))
-  expect_true("rule2" %in% names(auto_medianResult_data))
-  
-  # iterate through each row in the median column that is not NA
-  # if the median column has a value, the corresponding rule2 column should be TRUE
-  for (i in seq_len(nrow(auto_medianResult_data))){
-    if (!is.na(auto_medianResult_data$median[i])){
-      expect_true(auto_medianResult_data$rule2[i])
-    }
-  }
 })
 
 test_that("Median is not plotted when floatingMedian is set to auto and there is not a shift rule 2 break in the last floatingMedian_n rows", {
-  # Load test dataset whose last 12 points does not contain a shift rule 2 break 
+  # Load test dataset where last 12 points does not contain a shift rule 2 break 
   test_medianNoRule2 <- readRDS("testdata/test_medianNoRule2Breaks.rds")
   
-  #set the n points for the median
+  # Set the n points for the median
   test_median_n <- 12L
 
-  #view the data being plotted in XmR chart
+  # View the data being plotted in XmR chart
   chart_result <- autospc::plot_auto_SPC(df = test_medianNoRule2,
                                          chartType = 'XMR',
                                          floatingMedian = "auto",
@@ -136,5 +97,6 @@ test_that("Median is not plotted when floatingMedian is set to auto and there is
 
   chart_result_data <- chart_result$data
   
+  # Test that the median is not calculated not plotted when there is not a shift rule 2 break in last 12L points 
   expect_false("median" %in% names(chart_result_data))
 })

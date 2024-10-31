d548e7312bd7287952135dfa259c1dd20dc71ab7
Tom Woodcock <thomas.woodcock99@imperial.ac.uk>
2024-10-31 17:31:06 +0000

    Add tests for mr_screen_max_loops

--- tests/testthat/test_mr_chart.R
+++ tests/testthat/test_mr_chart.R
@@ -1,8 +1,11 @@
+library(testthat)
+
 #load test data 
 mr_data <- readRDS("testdata/test_mr_data.rds")
+extreme_mr_data <- readRDS("testdata/test_mr_data.rds")
 
-#test that mr control limits match the recalculated mr control limits
-library(testthat)
+#test that mr control limits match those from qicharts2
+# when mr_screen_max_loops = 0
 test_that("mR limits match the recalculated mR limits",{
   mrs = get_mrs(y = mr_data$y)
   results <- get_mr_limits(mr = mrs,
@@ -19,4 +22,26 @@ test_that("mR chart created without error",{
   expect_no_error(
     chart <- plot_auto_SPC(df = mr_data, chartType = "MR", plotChart = TRUE)
   )
+})
+
+test_that("mr_screen_max_loops makes no difference to mr chart limits",{
+  
+  results_table_0 <- plot_auto_SPC(df = extreme_mr_data,
+                                   chartType = "MR",
+                                   plotChart = FALSE,
+                                   mr_screen_max_loops = 0)
+  
+  results_table_1 <- plot_auto_SPC(df = extreme_mr_data,
+                                   chartType = "MR",
+                                   plotChart = FALSE,
+                                   mr_screen_max_loops = 1)
+  
+  results_table_inf <- plot_auto_SPC(df = extreme_mr_data,
+                                   chartType = "MR",
+                                   plotChart = FALSE,
+                                   mr_screen_max_loops = Inf)
+  
+  expect_equal(results_table_1, results_table_0)
+  expect_equal(results_table_inf, results_table_0)
+  
 })

83e1ab11c95ea5c1f51ea9275d56ce8424d1674c
HorridTom <woodcock.thomas@gmail.com>
2025-08-15 13:06:58 +0100

    Capture bug 149 (breakpoints relying on centre line value)

---
+++ tests/testthat/test_breakpoints_bug_149.R
@@ -0,0 +1,20 @@
+bug_148_data <- readRDS(file.path("testdata", "bug_148_data.rds"))
+# Bug 149 captured using same data as bug 148
+
+test_that(paste0("break points are inserted even with no change in centre line",
+                 "(bug 149 fixed)"), {
+                   
+                   result <- plot_auto_SPC(bug_148_data,
+                                           chartType = "C",
+                                           plotChart = FALSE)
+                   
+                   num_breakpoints = sum(result %>%
+                                           dplyr::filter(dplyr::row_number()
+                                                         != 1L) %>%
+                                           dplyr::pull(breakPoint))
+                   
+                   expect_equal(num_breakpoints,
+                                1L)
+                   
+                   
+                 })

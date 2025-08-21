ab19fab3ccf4347280574fa107543c8eaf30386e
HorridTom <woodcock.thomas@gmail.com>
2025-08-21 15:42:03 +0100

    Make minor improvements for clarity in I chart limit tests

--- tests/testthat/test_ichart_limit.R
+++ tests/testthat/test_ichart_limit.R
@@ -6,18 +6,17 @@ test_data <- readRDS("testdata/test_individual_data.rds")
 # test_individual_answer <- qicharts2::qic(x, y, data = test_data, chart = 'i',
 #                                           return.data = TRUE)
 # qicharts2 v.0.7.2
-test_individual_answer <- readRDS("testdata/test_individual_answer.rds")
+test_individual_answer <- readRDS(file.path("testdata",
+                                            "test_individual_answer.rds"))
 
 # test that the i chart limits is the same as qicharts2 results 
 test_that("I chart limits the same as live qicharts2 v.0.7.2",{
   results <- get_i_limits(y = test_data$y,
                           mr_screen_max_loops = 1)
   
-  correct_answers <- test_individual_answer
-  
-  expect_equal(results$cl, correct_answers$cl)
-  expect_equal(results$lcl, correct_answers$lcl)
-  expect_equal(results$ucl, correct_answers$ucl)
+  expect_equal(results$cl, test_individual_answer$cl)
+  expect_equal(results$lcl, test_individual_answer$lcl)
+  expect_equal(results$ucl, test_individual_answer$ucl)
   
 })
 
@@ -28,17 +27,16 @@ extreme_mr_data <- readRDS("testdata/test_mr_data.rds")
 # test_mr_answer <- qicharts2::qic(x, y, data = extreme_mr_data, chart = 'i',
 #                                   return.data = TRUE)
 # qicharts2 v.0.7.2
-test_mr_answer <- readRDS("testdata/test_mr_answer.rds")
+test_mr_answer <- readRDS(file.path("testdata",
+                                    "test_mr_answer.rds"))
 
 test_that("I chart limits the same as live qicharts2 v.0.7.2",{
   results <- get_i_limits(y = extreme_mr_data$y,
                           mr_screen_max_loops = 1)
   
-  correct_answers <- test_mr_answer
-  
-  expect_equal(results$cl, correct_answers$cl)
-  expect_equal(results$lcl, correct_answers$lcl)
-  expect_equal(results$ucl, correct_answers$ucl)
+  expect_equal(results$cl, test_mr_answer$cl)
+  expect_equal(results$lcl, test_mr_answer$lcl)
+  expect_equal(results$ucl, test_mr_answer$ucl)
   
 })
 

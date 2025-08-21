3f88a67d6662611649f26754f08593c1ba0880f9
HorridTom <woodcock.thomas@gmail.com>
2025-08-21 15:42:53 +0100

    Remove dependency on qicharts2 in I chart exclusion test

--- tests/testthat/test_ichart_exclusions.R
+++ tests/testthat/test_ichart_exclusions.R
@@ -5,22 +5,25 @@ test_data <- readRDS("testdata/test_individual_data.rds")
 #modify the data to include some extreme values
 test_data[6,2] <- 200
 test_data[15,2] <- 150
-
+# Correct answer created using:
+# test_ichart_exclusions_answer <- qicharts2::qic(x,
+#                                                 y,
+#                                                 data = test_data,
+#                                                 chart = 'i',
+#                                                 exclude = c(6, 15),
+#                                                 return.data = TRUE)                                         return.data = TRUE)
+# qicharts2 v.0.7.2
+test_ichart_exclusions_answer <- readRDS(
+  file.path("testdata",
+            "test_ichart_exclusions_answer.rds"))
 
 #test that the i chart limits is the same as qicharts2 results 
-test_that("I chart limits the same as live qicharts2 v.0.7.2",{
+test_that("I chart limits the same as qicharts2 v.0.7.2",{
   results <- get_i_limits(y = test_data$y,
                           exclusion_points = c(6, 15))
   
-  correct_answers <- qicharts2::qic(x,
-                                    y,
-                                    data = test_data,
-                                    chart = 'i',
-                                    exclude = c(6, 15),
-                                    return.data = TRUE)
-  
-  expect_equal(results$cl, correct_answers$cl)
-  expect_equal(results$lcl, correct_answers$lcl)
-  expect_equal(results$ucl, correct_answers$ucl)
+  expect_equal(results$cl, test_ichart_exclusions_answer$cl)
+  expect_equal(results$lcl, test_ichart_exclusions_answer$lcl)
+  expect_equal(results$ucl, test_ichart_exclusions_answer$ucl)
   
 })

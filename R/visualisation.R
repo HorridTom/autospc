97b01e9a195f1c166a6658f816ed6d268bf9dad5
HorridTom <woodcock.thomas@gmail.com>
2025-09-11 16:33:00 +0100

    Sort line lengths

--- R/visualisation.R
+++ R/visualisation.R
@@ -71,12 +71,17 @@ create_spc_plot <- function(df,
     
     #get x axis breaks
     if(is.null(x_break)) {
-      x_break <- as.numeric(difftime(as.Date(end_x), as.Date(start_x), units = "days")) / 40
+      x_break <- as.numeric(difftime(as.Date(end_x),
+                                     as.Date(start_x),
+                                     units = "days")) / 40
     }
     
     p <- p + ggplot2::scale_x_date(labels = scales::date_format(x_date_format),
-                                   breaks = seq(as.Date(start_x), as.Date(end_x), x_break),
-                                   limits = c(as.Date(start_x), as.Date(end_x))
+                                   breaks = seq(as.Date(start_x),
+                                                as.Date(end_x),
+                                                x_break),
+                                   limits = c(as.Date(start_x),
+                                              as.Date(end_x))
     )
     
   } else if(xType == "integer") {
@@ -134,15 +139,24 @@ create_timeseries_plot <- function(df,
                        linewidth = 0.5) +
     ggplot2::geom_point(colour = "black", size = 2) +
     ggplot2::theme(panel.grid.major.y = ggplot2::element_blank(),
-                   panel.grid.major.x = ggplot2::element_line(colour = "grey80"),
+                   panel.grid.major.x = ggplot2::element_line(
+                     colour = "grey80"
+                     ),
                    panel.grid.minor = ggplot2::element_blank(),
                    panel.background = ggplot2::element_blank(),
-                   axis.text.x = ggplot2::element_text(angle = 45, hjust = 1, vjust = 1.0, size = 14),
-                   axis.text.y = ggplot2::element_text(size = 14), axis.title = ggplot2::element_text(size = 14),
-                   plot.title = ggplot2::element_text(size = 20, hjust = 0),
-                   plot.subtitle = ggplot2::element_text(size = 16, face = "italic"),
+                   axis.text.x = ggplot2::element_text(angle = 45,
+                                                       hjust = 1,
+                                                       vjust = 1.0,
+                                                       size = 14),
+                   axis.text.y = ggplot2::element_text(size = 14),
+                   axis.title = ggplot2::element_text(size = 14),
+                   plot.title = ggplot2::element_text(size = 20,
+                                                      hjust = 0),
+                   plot.subtitle = ggplot2::element_text(size = 16,
+                                                         face = "italic"),
                    axis.line = ggplot2::element_line(colour = "grey60"),
-                   plot.caption = ggplot2::element_text(size = 10, hjust = 0.5)) +
+                   plot.caption = ggplot2::element_text(size = 10,
+                                                        hjust = 0.5)) +
     ggplot2::ggtitle(title,
                      subtitle = subtitle) +
     ggplot2::labs(x = override_x_title,
@@ -157,14 +171,22 @@ create_timeseries_plot <- function(df,
 }
 
 
-format_SPC <- function(cht, df, r1_col, r2_col, ymin, ymax) {
-  point_colours <- c("Rule 1" = r1_col, "Rule 2" = r2_col, 
-                     "None" = "black", "Excluded from limits calculation" = "grey")
+format_SPC <- function(cht,
+                       df,
+                       r1_col,
+                       r2_col,
+                       ymin,
+                       ymax) {
+  point_colours <- c("Rule 1" = r1_col,
+                     "Rule 2" = r2_col, 
+                     "None" = "black",
+                     "Excluded from limits calculation" = "grey")
   
   #get exemplar calculation and display periods
   plot_periods <- df$plotPeriod
   
-  first_display_period <- plot_periods[grep("display", plot_periods)[1]]
+  first_display_period <- plot_periods[grep("display",
+                                            plot_periods)[1]]
   first_calc_period <- plot_periods[1]
   
   suppressWarnings( # to avoid the warning about using alpha for discrete vars
@@ -192,9 +214,11 @@ format_SPC <- function(cht, df, r1_col, r2_col, ymin, ymax) {
                          linewidth = 0.5,
                          show.legend = FALSE,
                          na.rm = TRUE) +
-      ggplot2::geom_point(ggplot2::aes(colour = highlight), size = 2,
+      ggplot2::geom_point(ggplot2::aes(colour = highlight),
+                          size = 2,
                           na.rm = TRUE) +
-      ggplot2::scale_color_manual("Rule triggered*", values = point_colours) + 
+      ggplot2::scale_color_manual("Rule triggered*",
+                                  values = point_colours) + 
       ggplot2::scale_alpha_discrete("Period Type",
                                     labels = if(!is.na(first_display_period)) {
                                       c("Calculation", "Display")
@@ -213,7 +237,10 @@ format_SPC <- function(cht, df, r1_col, r2_col, ymin, ymax) {
                                       c(first_calc_period)
                                     },
                                     guide = ggplot2::guide_legend(
-                                      override.aes = list(alpha = if(!is.na(first_display_period)) {
+                                      override.aes = list(
+                                        alpha = if(
+                                          !is.na(first_display_period)
+                                          ) {
                                         c(1, 0.4)
                                       } else {
                                         c(1)
@@ -222,15 +249,24 @@ format_SPC <- function(cht, df, r1_col, r2_col, ymin, ymax) {
                                     )
       ) +
       ggplot2::theme(panel.grid.major.y = ggplot2::element_blank(),
-                     panel.grid.major.x = ggplot2::element_line(colour = "grey80"),
+                     panel.grid.major.x = ggplot2::element_line(
+                       colour = "grey80"
+                       ),
                      panel.grid.minor = ggplot2::element_blank(),
                      panel.background = ggplot2::element_blank(),
-                     axis.text.x = ggplot2::element_text(angle = 45, hjust = 1, vjust = 1.0, size = 14),
-                     axis.text.y = ggplot2::element_text(size = 14), axis.title = ggplot2::element_text(size = 14),
-                     plot.title = ggplot2::element_text(size = 20, hjust = 0),
-                     plot.subtitle = ggplot2::element_text(size = 16, face = "italic"),
+                     axis.text.x = ggplot2::element_text(angle = 45,
+                                                         hjust = 1,
+                                                         vjust = 1.0,
+                                                         size = 14),
+                     axis.text.y = ggplot2::element_text(size = 14),
+                     axis.title = ggplot2::element_text(size = 14),
+                     plot.title = ggplot2::element_text(size = 20,
+                                                        hjust = 0),
+                     plot.subtitle = ggplot2::element_text(size = 16,
+                                                           face = "italic"),
                      axis.line = ggplot2::element_line(colour = "grey60"),
-                     plot.caption = ggplot2::element_text(size = 10, hjust = 0.5)) 
+                     plot.caption = ggplot2::element_text(size = 10,
+                                                          hjust = 0.5)) 
   )
 }
 

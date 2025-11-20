6d942e58faef220d7194d86408a9b59f519071dd
HorridTom <woodcock.thomas@gmail.com>
2025-11-20 10:12:30 +0000

    Additional tests to capture use of x_break when x is POSIXct

--- tests/testthat/test_posixct_x.R
+++ tests/testthat/test_posixct_x.R
@@ -13,3 +13,31 @@ test_that("Chart is created without error when x-axis is POSIXct", {
   )
   
 })
+
+
+test_that("x_break argument accepts difftime when x-axis is POSIXct", {
+  
+  expect_no_error(
+    plot_auto_SPC(POSIXct_x_data,
+                  chartType = "C'",
+                  x = Month,
+                  y = Total,
+                  x_break = as.difftime(6, units = "weeks")),
+    message = "the condition has length"
+  )
+  
+})
+
+
+test_that("Error thrown when x_break not difftime and x-axis is POSIXct", {
+  
+  expect_error(
+    plot_auto_SPC(POSIXct_x_data,
+                  chartType = "C'",
+                  x = Month,
+                  y = Total,
+                  x_break = 6),
+    regexp = "x_break as a difftime"
+  )
+  
+})

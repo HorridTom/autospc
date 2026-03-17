d3b4135b2d0464eba2a79878d6892e0fd3071f56
HorridTom <woodcock.thomas@gmail.com>
2026-03-17 17:10:01 +0000

    Add test for binary data aggregation (for P chart)

---
+++ tests/testthat/test_aggregate_data.R
@@ -0,0 +1,40 @@
+# Simulate data for aggregation testing
+# 1. binary outcome for p(')-charts
+# set.seed(202602)
+# 
+# x <- rep(c(1L:42L),
+#          times = rep(c(25L,50L),
+#                      c(21L,21L)))
+# 
+# y <- runif(n = length(x),
+#            min = 0,
+#            max = 1) <= 0.5
+# 
+# test_pchart_aggregation_data <- tibble::tibble(x = x,
+#                                                y = y)
+# 
+# saveRDS(test_pchart_aggregation_data,
+#         file = "tests/testthat/testdata/test_pchart_aggregation_data.rds")
+
+test_pchart_aggregation_data <- readRDS(
+  file.path("testdata",
+            "test_pchart_aggregation_data.rds"))
+
+# Correct aggregation produced using pivot table in Excel
+test_pchart_aggregated_data <- readRDS(
+  file.path("testdata",
+            "test_pchart_aggregated_data.rds"))
+
+test_that("binary aggregation works", {
+  
+  aggregated_data <- aggregate_data(df = test_pchart_aggregation_data,
+                                    chart_type = "P")
+  
+  expect_equal(aggregated_data$x,
+               test_pchart_aggregated_data$x)
+  expect_equal(aggregated_data$y,
+               test_pchart_aggregated_data$y)
+  expect_equal(aggregated_data$n,
+               test_pchart_aggregated_data$n)
+
+})
